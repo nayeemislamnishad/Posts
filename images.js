86a21481176f360bc31f2ac6150622776f3a5d01
crakknayeem <159826869+crakknayeem@users.noreply.github.com>
2024-02-20 22:32:44 +0600

    Update images.js

--- images.js
+++ images.js
@@ -1,8 +1,8 @@
 // posts.js
 var postInfo = [
-    { filename: 'post1', images: ['IMAGES/image1.jpg'] },
-    { filename: 'post2', images: ['IMAGES/image2sub1.png', 'IMAGES/image2sub2.gif'] },
-    { filename: 'post3', images: ['IMAGES/image3.jpg'] },
+  /*  { filename: 'post1', images: ['IMAGES/image1.jpg'] },*/
+    { filename: 'post2', images: ['IMAGES/image2sub1.jpg', 'IMAGES/image2sub2.jpg'] },
+   /* { filename: 'post3', images: ['IMAGES/image3.jpg'] }, */
     { filename: 'post4' }, // No images for post4
     // Add more posts with their respective images or without images
 ];
