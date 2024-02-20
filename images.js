b4893bfc49cedc6453c01baf32c75facaaa36df8
crakknayeem <159826869+crakknayeem@users.noreply.github.com>
2024-02-20 22:17:26 +0600

    Create images.js

---
+++ images.js
@@ -0,0 +1,8 @@
+// posts.js
+var postInfo = [
+    { filename: 'post1', images: ['image1.jpg'] },
+    { filename: 'post2', images: ['image2sub1.png', 'image2sub2.gif'] },
+    { filename: 'post3', images: ['image3.jpg'] },
+    { filename: 'post4' }, // No images for post4
+    // Add more posts with their respective images or without images
+];
