1c22853dab93c282cca9efce4293a2625fae08ec
crakknayeem <159826869+crakknayeem@users.noreply.github.com>
2024-02-22 05:12:12 +0600

    Update vediolink.js

--- vediolink.js
+++ vediolink.js
@@ -1,6 +1,6 @@
 const videoLinks = [
         "https://youtu.be/GvXDq-P1NB8?si=mKG1nAdZ5F9FddcM",
         "https://youtu.be/D4B7xO1HVEA?si=zXhEzbKm6OJxVXri",
-        "https://www.youtube.com/watch?v=VIDEO_ID_3",
+        "https://youtu.be/XhO_3B8xBKQ?si=2QtT-NCjrPhO7jdp",
         // Add more video links as needed
     ];
