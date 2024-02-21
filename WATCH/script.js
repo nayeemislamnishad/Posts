ef38e7d309b198d4ca83dda95c7cfc9da418f0b6
crakknayeem <159826869+crakknayeem@users.noreply.github.com>
2024-02-22 04:54:20 +0600

    Update script.js

--- WATCH/script.js
+++ WATCH/script.js
@@ -1,10 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
+  
+    /*
     const videoLinks = [
         "https://youtu.be/GvXDq-P1NB8?si=mKG1nAdZ5F9FddcM",
         "https://youtu.be/D4B7xO1HVEA?si=zXhEzbKm6OJxVXri",
         "https://www.youtube.com/watch?v=VIDEO_ID_3",
         // Add more video links as needed
     ];
+
+    */
 
     const videosContainer = document.getElementById("videos-container");
     displayVideos(videoLinks);
