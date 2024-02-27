9ffcb2536b0551c4021a2fcc2fac109e7fcdac4c
crakknayeem <159826869+crakknayeem@users.noreply.github.com>
2024-02-27 10:06:45 +0600

    Update youtube_vedios_controller.js

--- interface/youtube_vedios_controller.js
+++ interface/youtube_vedios_controller.js
@@ -1,20 +1,5 @@
 // Your JavaScript array containing YouTube video IDs
-        const videoIds = [
-            "752Ziu39zTE",
-            "P-4zG8uTuHs",
-            "XbNQhiLmJTc",
-            "FlCTifxUl1s",
-            "jvk9v806gzY",
-            "-UdfM1xxXJk",
-            "umEaa_6rZGo",
-            "c3CAPHfOZDY",
-            "V-oqPysfoiw",
-            "ALn_ch0aqB0",
-          
-            
-            // Add more video IDs as needed
-        ];
-
+        
         // Function to create iframe for each video ID
         // Function to create iframe for each video ID
 function createVideoIframe(videoId) {
