b42b0f5934d9fc1c083938d9707eda92e265a61d
crakknayeem <159826869+crakknayeem@users.noreply.github.com>
2024-03-07 14:02:41 +0600

    Update and rename vediolink.js to vedio_links.js

---
+++ FRESH/vedio_links.js
@@ -0,0 +1,12 @@
+const videoIds = [
+
+        "XbdaG94VavY",
+        "XhO_3B8xBKQ",
+        "5hNNbHCrTPI",
+        "752Ziu39zTE",
+        
+        
+        // Add more video links as needed
+    
+            // Add more video IDs as needed
+        ];
--- FRESH/vediolink.js
+++
@@ -1,9 +0,0 @@
-const videoLinks = [
-        "https://youtu.be/XbdaG94VavY?si=PyRmOnJUe00u8wDV",
-        "https://youtu.be/XhO_3B8xBKQ?si=2QtT-NCjrPhO7jdp",
-        "https://youtube.com/shorts/5hNNbHCrTPI?si=coznvzMV2z8jEZ0g",
-        "https://www.youtube.com/live/752Ziu39zTE?si=T4NoPUehBNzTMtBl",
-        
-        
-        // Add more video links as needed
-    ];
