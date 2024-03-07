f96f8717b1236016f5889637533e1833b9eb6a97
crakknayeem <159826869+crakknayeem@users.noreply.github.com>
2024-03-07 21:04:29 +0600

    Update vedio_links.js

--- FRESH/vedio_links.js
+++ FRESH/vedio_links.js
@@ -3,6 +3,8 @@ const videoIds = [
         "dBHenPVKg7c",
         "gTCIcPpydgU",
         "zc-3f37Aa5Q",
+        "kZGpkkfk2lA",
+        "3OYrJWVx7F0",
         
         // Add more video links as needed
     
