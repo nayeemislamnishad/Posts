6df2d8e9d09a15db0b1755c8c1f89d316c1e7a18
crakknayeem <159826869+crakknayeem@users.noreply.github.com>
2024-03-08 12:24:33 +0600

    Update text_and_image_controller.js

--- interface/text_and_image_controller.js
+++ interface/text_and_image_controller.js
@@ -1,3 +1,38 @@
+xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+        var postContainer = document.createElement('div');
+        postContainer.className = 'post-container';
+
+        var hrElement = document.createElement('h1');
+        hrElement.innerText = 'Posts........';
+        postContainer.appendChild(hrElement);
+
+        var textElement = document.createElement('p');
+        textElement.innerHTML = xhr.responseText; // Use innerHTML to render HTML content
+        postContainer.appendChild(textElement);
+
+        // Extract and append images from the text content
+        var textImages = xhr.responseText.match(/src\s*=\s*['"]([^'"]+)['"]/g);
+        if (textImages) {
+            textImages.forEach(function (match) {
+                var src = match.match(/src\s*=\s*['"]([^'"]+)['"]/)[1];
+                var imageElement = document.createElement('img');
+                imageElement.src = 'IMAGES/' + src; // Adjust the path accordingly
+                imageElement.alt = 'Description of your image';
+                postContainer.appendChild(imageElement);
+            });
+        }
+
+        postsContainer.appendChild(postContainer);
+    }
+};
+
+
+
+
+
+
+/* previous 
 
 xhr.onreadystatechange = function () {
     if (xhr.readyState === 4 && xhr.status === 200) {
@@ -40,9 +75,9 @@ xhr.onreadystatechange = function () {
 
 
 
+*/
 
-
-/*
+/* original 
 
 function loadTextFiles() {
     var postsContainer = document.getElementById('posts-container');
