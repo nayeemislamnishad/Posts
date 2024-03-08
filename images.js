64a6a6b0850a9a340aa1368a2e7ab17d17832866
crakknayeem <159826869+crakknayeem@users.noreply.github.com>
2024-03-08 12:37:10 +0600

    Update images.js

--- images.js
+++ images.js
@@ -1,9 +1,8 @@
-// posts.js
 var postInfo = [
    { filename: 'post1'},
    { filename: 'post2' },
    { filename: 'post3'},
-   { filename: 'post4' }, // No images for post4
+   { filename: 'post4' },
    { filename: 'post5' },
    { filename: 'post6' },
    { filename: 'post7' },
