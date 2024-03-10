6e9823cc8066d7d9264f52125903e3fd1ae3fdd8
crakknayeem <159826869+crakknayeem@users.noreply.github.com>
2024-03-11 00:28:49 +0600

    Update images.js

--- images.js
+++ images.js
@@ -1,5 +1,3 @@
-
-
 var postInfoo = [
    { filename: 'post1', date: '04-03-2024' },
    { filename: 'post2', date: '09-03-2024' },
@@ -13,10 +11,16 @@ var postInfoo = [
 
 // Clone with the date formatted as year-month-day
 var postInfo = postInfoo.map(post => {
-    const [day, month, year] = post.date.split('-');
-    const formattedDate = `${year}-${month}-${day}`;
-    return { filename: post.filename, date: formattedDate };
+   const [day, month, year] = post.date.split('-');
+   const formattedDate = `${year}-${month}-${day}`;
+   return { filename: post.filename, date: formattedDate };
 });
 
-postInfo.sort((a, b) => new Date(a.date) - new Date(b.date));
-console.log(postInfo);
+// Sort the postInfo array based on the date in descending order (newest to oldest)
+postInfo.sort((a, b) => new Date(b.date) - new Date(a.date));
+
+// Reverse the order to display posts from newest to oldest
+
+
+// Display the reversed postInfo array in the console
+console.log("sorted luicca ",postInfo);
